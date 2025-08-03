4bd386b85f68a617f2afb6501f75ad332de31105
rebodutch <rebodutchman@gmail.com>
2025-08-03 07:44:13 +0000

    refactor(log): unfiy python log format between python services and golang service

--- logging/baselogger.py
+++ logging/baselogger.py
@@ -1,14 +1,27 @@
 import sys
+import time
 import logging
 import contextvars
-from logging import StreamHandler
 
-from pythonjsonlogger.json import JsonFormatter # type: ignore 
+from logging import StreamHandler
+from datetime import datetime, timezone
+
+from pythonjsonlogger.json import JsonFormatter  # type: ignore
 
 trace_id_var = contextvars.ContextVar("trace_id", default="-")
 log_fields = [
-    "timestamp", "levelname", "service", "service_version", "module", "event", "trace_id", "message", "extra"
+    "timestamp",
+    "ts",
+    "levelname",
+    "service",
+    "service_version",
+    "module",
+    "event",
+    "trace_id",
+    "message",
+    "extra",
 ]
+
 
 class ServiceInfoFilter(logging.Filter):
     def __init__(self, service_name, service_version):
@@ -17,10 +30,13 @@ class ServiceInfoFilter(logging.Filter):
         self.service_version = service_version
 
     def filter(self, record):
+        record.ts = time.time()
+        record.timestamp = datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
         record.trace_id = trace_id_var.get()
         record.service = self.service_name
         record.service_version = self.service_version
         return True
+
 
 def setup_logger(log_level: str, service_name: str, service_version: str):
     logger = logging.getLogger()
@@ -28,11 +44,14 @@ def setup_logger(log_level: str, service_name: str, service_version: str):
 
     json_handler = StreamHandler(sys.stdout)
     json_formatter = JsonFormatter(
-        fmt=' '.join([f'%({field})s' for field in log_fields]),
-        rename_fields={"asctime": "timestamp"},
+        fmt=" ".join([f"%({field})s" for field in log_fields]),
+        # To ensure cross-language log compatibility with tools like Loki,
+        # this logger is configured to replicate the field structure of the
+        # popular Golang Zap logger (e.g., 'level', 'msg').
+        rename_fields={"levelname": "level", "message": "msg"},
     )
     json_handler.setFormatter(json_formatter)
     json_handler.addFilter(ServiceInfoFilter(service_name, service_version))
     logger.addHandler(json_handler)
-    
+
     logger.addFilter(ServiceInfoFilter(service_name, service_version))
